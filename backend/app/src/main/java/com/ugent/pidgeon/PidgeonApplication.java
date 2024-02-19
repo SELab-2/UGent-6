a7c592922aac16895d5510885c0f5b85ccceca76
AWerbrouck <arthur.jasaw@gmail.com>
2024-02-19 18:40:22 +0100

    adding web support

---
+++ backend/app/src/main/java/com/ugent/pidgeon/PidgeonApplication.java
@@ -0,0 +1,13 @@
+package com.ugent.pidgeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidgeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidgeonApplication.class, args);
+	}
+
+}
---
+++ backend/app/src/test/java/com/ugent/pidgeon/PidgeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugent.pidgeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidgeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
