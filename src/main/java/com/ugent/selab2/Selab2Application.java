6b89c442d31c30f1ad4122e27c07aca231d307db
AWerbrouck <arthur.jasaw@gmail.com>
2024-02-19 14:10:03 +0100

    setting up the project in java spring + gradle

---
+++ src/main/java/com/ugent/selab2/Selab2Application.java
@@ -0,0 +1,13 @@
+package com.ugent.selab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Selab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Selab2Application.class, args);
+	}
+
+}
