8e570629dddac620e5a1e57bdc51fa24f99cff7f
Arne Dierick <arne.dierick@ugent.be>
2024-03-04 16:28:26 +0100

    tests should work now

--- backend/app/src/main/java/com/ugent/pidgeon/PidgeonApplication.java
+++ backend/app/src/main/java/com/ugent/pidgeon/PidgeonApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.ugent.pidgeon","com.ugent.selab2"})
 public class PidgeonApplication {
 
 	@RequestMapping("/")
