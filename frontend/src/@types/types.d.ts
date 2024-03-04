63ea7434e79b040d6400fec260fc93188d344071
usserwoutV2 <wout.verdyck@gmail.com>
2024-03-04 20:29:43 +0100

    Updated msal config

---
+++ frontend/src/@types/types.d.ts
@@ -0,0 +1,12 @@
+
+
+
+
+declare global {
+  interface Window {
+   
+
+  }
+}
+
+export {}
--- frontend/src/auth/AuthConfig.ts
+++ frontend/src/auth/AuthConfig.ts
@@ -1,9 +1,13 @@
 import { Configuration, PopupRequest } from "@azure/msal-browser";
 
+
+const hostedOnServer = window.location.hostname === "sel2-6.ugent.be" // For now, gotta change this when ugent auth is fixed
+
 // Config object to be passed to Msal on creation
 export const msalConfig: Configuration = {
     auth: {
-        clientId: "39136cda-f02f-4305-9b08-45f132bab07e",
+        // 39136cda-f02f-4305-9b08-45f132bab07e
+        clientId: hostedOnServer ? "83616890-6249-4726-9e0c-87da4dde75b0" :"39136cda-f02f-4305-9b08-45f132bab07e" ,
         //For UGent auth: "https://login.microsoftonline.com/d7811cde-ecef-496c-8f91-a1786241b99c",
         authority: "https://login.microsoftonline.com/d7811cde-ecef-496c-8f91-a1786241b99c",  //  "https://login.microsoftonline.com/62835335-e5c4-4d22-98f2-9d5b65a06d9d", 
         redirectUri: "/",
