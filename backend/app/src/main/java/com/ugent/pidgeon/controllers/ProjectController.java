2fb4b1b96490e2440df23b769b7acd7631d636ac
Arthur Werbrouck <arthur.jasaw@gmail.com>
2024-03-08 14:06:33 +0100

    project routes

--- backend/app/src/main/java/com/ugent/pidgeon/controllers/ProjectController.java
+++ backend/app/src/main/java/com/ugent/pidgeon/controllers/ProjectController.java
@@ -6,22 +6,43 @@ import com.ugent.pidgeon.postgre.models.TestEntity;
 import com.ugent.pidgeon.postgre.repository.ProjectRepository;
 import com.ugent.pidgeon.postgre.repository.TestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
-public class JpaProjectController {
+public class ProjectController {
     @Autowired
     private ProjectRepository projectRepository;
 
     @Autowired
     private TestRepository testRepository;
 
-    @GetMapping("/api/temp/projects")
+    @GetMapping("/api/projects/{projectId}")
+    public ResponseEntity<ProjectEntity> getProjectById(@PathVariable Long projectId) {
+        return projectRepository.findById(projectId)
+                .map(project -> ResponseEntity.ok().body(project)) // Return the project as JSON
+                .orElseGet(() -> ResponseEntity.notFound().build()); // Or return 404 if not found
+    }
+
+    @GetMapping("/api/projects/{courseId}")
+    public ResponseEntity<List<ProjectEntity>> getProjectByCourseId(@PathVariable Long courseId) {
+        List<ProjectEntity> projects = projectRepository.findByCourseId(courseId);
+        if (projects.isEmpty()) {
+            return ResponseEntity.notFound().build(); // Or return an empty list, based on your preference
+        }
+        return ResponseEntity.ok(projects);
+    }
+
+
+
+    @GetMapping("/api/projects")
     public List<String> getProjects() {
         List<String> res = new ArrayList<>();
         for (ProjectEntity project : projectRepository.findAll()) {
@@ -36,4 +57,5 @@ public class JpaProjectController {
         }
         return res;
     }
+
 }
