b9d791a8c1bbf49975751af1d4e86ca0170357c2
Arthur Werbrouck <arthur.jasaw@gmail.com>
2024-03-10 19:23:21 +0100

    adding deadline base path

--- backend/app/src/main/java/com/ugent/pidgeon/controllers/ApiRoutes.java
+++ backend/app/src/main/java/com/ugent/pidgeon/controllers/ApiRoutes.java
@@ -3,7 +3,7 @@ package com.ugent.pidgeon.controllers;
 public final class ApiRoutes {
     public static final String USER_BASE_PATH = "/api/users";
     public static final String COURSE_BASE_PATH = "/api/courses";
-
+    public static final String DEADLINE_BASE_PATH = "/api/deadlines";
     public static final String PROJECT_BASE_PATH = "/api/projects";
 
     public static final String GROUP_BASE_PATH = "/api/groups";
