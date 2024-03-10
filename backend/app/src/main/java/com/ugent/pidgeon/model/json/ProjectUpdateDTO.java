8f2e47f17c76eee5c615622c36343f7f81efb4f5
Arthur Werbrouck <arthur.jasaw@gmail.com>
2024-03-10 19:11:45 +0100

    put api/projects/<projectId> working

---
+++ backend/app/src/main/java/com/ugent/pidgeon/model/json/ProjectUpdateDTO.java
@@ -0,0 +1,34 @@
+package com.ugent.pidgeon.model.json;
+
+import java.sql.Timestamp; // Import Timestamp
+
+public class ProjectUpdateDTO {
+    private String name;
+    private String description;
+    private Timestamp deadline;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Timestamp deadline) {
+        this.deadline = deadline;
+    }
+}
