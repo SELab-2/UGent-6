e8e275336b5e8d3ad3e7b19f8479a02a0d2adece
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2024-03-11 12:22:26 +0100

    Changed name of TestController

--- backend/app/src/main/java/com/ugent/pidgeon/controllers/TestController.java
+++ backend/app/src/main/java/com/ugent/pidgeon/controllers/TestController.java
@@ -19,17 +19,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
-public class JpaProjectTestController {
+public class TestController {
 
     @Autowired
     private ProjectRepository projectRepository;
