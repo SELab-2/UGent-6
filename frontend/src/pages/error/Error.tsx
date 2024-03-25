032f6f0258974fea4e07bb752be0fd5b6a15eb1f
Arne Dierick <arne.dierick@ugent.be>
2024-03-25 19:03:32 +0100

    afbeeldingen toegevoegd.

--- frontend/src/pages/error/Error.tsx
+++ frontend/src/pages/error/Error.tsx
@@ -1,7 +1,8 @@
 import React  from "react";
 import { AppRoutes } from "../../@types/routes"
 import {useTranslation} from "react-i18next";
-import {Button, Space, Typography} from "antd";
+import {Button, Result, Space, Typography} from "antd";
+
 
 interface ErrorPageProps {
     errorCode?: number;
@@ -13,6 +14,14 @@ const Error: React.FC<ErrorPageProps> = ({ errorCode, errorMessage }) => {
     const { Title, Text } = Typography;
 
     let title:string = "";
+    let status: "403" | "404" | "500"  = "404";
+
+    // Mapping van errorCode naar status
+    const statusMapping: { [key: number]: "403" | "404" | "500" } = {
+        403: "403",
+        404: "404",
+        500: "500"
+    };
 
     // Add default message to error page when no error message was provided
     if(errorMessage === undefined){
@@ -21,18 +30,20 @@ const Error: React.FC<ErrorPageProps> = ({ errorCode, errorMessage }) => {
     // Add default error title if no error code was provided, otherwise set it to error code
     if(errorCode === undefined){
         title = t("error.title")
+
     }else{
         title = errorCode.toString()
+        status = statusMapping[errorCode] || "404"
     }
 
     return (
-        <div>
+        <Result status={status}>
             <Space direction="vertical">
                 <Title>{title}</Title>
                 <Text>{errorMessage}</Text>
                 <Button type="primary" href={AppRoutes.HOME}>{t("error.homepage")}</Button>
             </Space>
-        </div>
+        </Result>
     );
 }
 
