4461d7aca3bc9d7d1ce37f4afe36ef7dd3bcba70
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2024-04-08 13:30:42 +0200

    Added PUT/PATCH for user (admin only)

--- backend/app/src/main/java/com/ugent/pidgeon/controllers/UserController.java
+++ backend/app/src/main/java/com/ugent/pidgeon/controllers/UserController.java
@@ -2,18 +2,19 @@ package com.ugent.pidgeon.controllers;
 
 import com.ugent.pidgeon.auth.Roles;
 import com.ugent.pidgeon.model.Auth;
-import com.ugent.pidgeon.model.json.CourseWithRelationJson;
 import com.ugent.pidgeon.model.json.UserJson;
+import com.ugent.pidgeon.model.json.UserUpdateJson;
 import com.ugent.pidgeon.postgre.models.UserEntity;
 import com.ugent.pidgeon.postgre.models.types.UserRole;
 import com.ugent.pidgeon.postgre.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.List;
 
 @RestController
@@ -60,5 +61,94 @@ public class UserController {
         return ResponseEntity.ok().body(userJson);
     }
 
+    private static final String EMAIL_REGEX =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
+    }
+    private CheckResult checkUserUpdateJson(UserUpdateJson json) {
+        if (json.getName() == null || json.getSurname() == null || json.getEmail() == null || json.getRole() == null) {
+            return new CheckResult(HttpStatus.BAD_REQUEST, "name, surname, email and role are required");
+        }
+
+        if (json.getRoleAsEnum() == null) {
+            return new CheckResult(HttpStatus.BAD_REQUEST, "Role is not valid: must be either student, admin or teacher");
+        }
+
+        if (json.getName().isBlank()) {
+            return new CheckResult(HttpStatus.BAD_REQUEST, "Name cannot be empty");
+        }
+
+        if (json.getSurname().isBlank()) {
+            return new CheckResult(HttpStatus.BAD_REQUEST, "Surname cannot be empty");
+        }
+
+        if (!isValidEmail(json.getEmail())) {
+            return new CheckResult(HttpStatus.BAD_REQUEST, "Email is not valid");
+        }
+
+        return new CheckResult(HttpStatus.OK, "");
+    }
+
+    private ResponseEntity<?> doUserUpdate(UserEntity user, UserUpdateJson json) {
+        user.setName(json.getName());
+        user.setSurname(json.getSurname());
+        user.setEmail(json.getEmail());
+        user.setRole(json.getRoleAsEnum());
+        userRepository.save(user);
+        return ResponseEntity.ok().body(new UserJson(user));
+    }
+
+    @PutMapping(ApiRoutes.USER_BASE_PATH + "/{userid}")
+    @Roles({UserRole.admin})
+    public ResponseEntity<?> updateUserById(@PathVariable("userid") Long userid, @RequestBody UserUpdateJson userUpdateJson, Auth auth) {
+        UserEntity user = userRepository.findById(userid).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        CheckResult checkResult = checkUserUpdateJson(userUpdateJson);
+        if (checkResult.getStatus() != HttpStatus.OK) {
+            return ResponseEntity.status(checkResult.getStatus()).body(checkResult.getMessage());
+        }
+
+        return doUserUpdate(user, userUpdateJson);
+    }
+
+    @PatchMapping(ApiRoutes.USER_BASE_PATH + "/{userid}")
+    @Roles({UserRole.admin})
+    public ResponseEntity<?> patchUserById(@PathVariable("userid") Long userid, @RequestBody UserUpdateJson userUpdateJson, Auth auth) {
+        UserEntity user = userRepository.findById(userid).orElse(null);
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        if (userUpdateJson.getName() == null) {
+            userUpdateJson.setName(user.getName());
+        }
+        if (userUpdateJson.getSurname() == null) {
+            userUpdateJson.setSurname(user.getSurname());
+        }
+        if (userUpdateJson.getEmail() == null) {
+            userUpdateJson.setEmail(user.getEmail());
+        }
+
+        Logger.getGlobal().info(userUpdateJson.getRole());
+        if (userUpdateJson.getRole() == null) {
+            userUpdateJson.setRole(user.getRole().toString());
+        }
+
+        CheckResult checkResult = checkUserUpdateJson(userUpdateJson);
+        if (checkResult.getStatus() != HttpStatus.OK) {
+            return ResponseEntity.status(checkResult.getStatus()).body(checkResult.getMessage());
+        }
+
+        return doUserUpdate(user, userUpdateJson);
+    }
 
 }
