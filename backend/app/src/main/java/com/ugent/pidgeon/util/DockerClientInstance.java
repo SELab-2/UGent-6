c78e8d10cb87c6cd1bca0d7db5587ef0e77bd178
Tristan Verbeken <tristan.verbeken@ugent.be>
2024-04-10 10:00:36 +0200

    fixed gradle build issues, and used different client builder so hopefully windows works too.

--- backend/app/src/main/java/com/ugent/pidgeon/util/DockerClientInstance.java
+++ backend/app/src/main/java/com/ugent/pidgeon/util/DockerClientInstance.java
@@ -1,8 +1,13 @@
 package com.ugent.pidgeon.util;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.netty.NettyDockerCmdExecFactory;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+
+import java.time.Duration;
 
 public class DockerClientInstance {
 
@@ -13,8 +18,17 @@ public class DockerClientInstance {
     }
 
     public static synchronized DockerClient getInstance() {
+
         if (dockerClient == null) {
-            dockerClient = DockerClientBuilder.getInstance().withDockerCmdExecFactory(new NettyDockerCmdExecFactory()).build();
+            DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+            DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                    .dockerHost(config.getDockerHost())
+                    .sslConfig(config.getSSLConfig())
+                    .maxConnections(100)
+                    .connectionTimeout(Duration.ofSeconds(30))
+                    .responseTimeout(Duration.ofSeconds(45))
+                    .build();
+            dockerClient = DockerClientImpl.getInstance(config, httpClient);
         }
         return dockerClient;
     }
