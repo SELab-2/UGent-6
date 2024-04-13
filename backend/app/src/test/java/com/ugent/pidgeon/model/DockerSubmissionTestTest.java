0786bdebb8bd6c50cd03b556c03474cc8f7d4688
Tristan Verbeken <tristan.verbeken@ugent.be>
2024-04-13 14:11:20 +0200

    Everything correctly working from docker submission testing

--- backend/app/src/test/java/com/ugent/pidgeon/model/DockerSubmissionTestTest.java
+++ backend/app/src/test/java/com/ugent/pidgeon/model/DockerSubmissionTestTest.java
@@ -1,16 +1,31 @@
 package com.ugent.pidgeon.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.ugent.pidgeon.model.submissionTesting.DockerSubmissionTestModel;
+import com.ugent.pidgeon.model.submissionTesting.DockerSubtestResult;
 import com.ugent.pidgeon.model.submissionTesting.DockerTestOutput;
+import java.io.File;
+import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class DockerSubmissionTestTest {
 
+    File initTestFile(String text, String fileName) {
+        String localFileLocation = System.getProperty("user.dir") + "/tmp/test/" + fileName;
+        File file = new File(localFileLocation);
+        try {
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+            FileUtils.writeStringToFile(file, text, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
 
     // Check if we can catch the console output of a script.
     @Test
@@ -34,6 +49,7 @@ public class DockerSubmissionTestTest {
         DockerTestOutput to = stm.runSubmission("echo 'PUSH DENIED' > /output/testOutput");
         assertFalse(to.allowed);
     }
+
     @Test
     void catchesConsoleLogs() throws InterruptedException {
         DockerSubmissionTestModel.addDocker("alpine:latest");
@@ -53,21 +69,56 @@ public class DockerSubmissionTestTest {
         // Load docker container
         DockerSubmissionTestModel stm = new DockerSubmissionTestModel("alpine");
 
-        // Create a input file in tmp/test/input
-        String localFileLocation = System.getProperty("user.dir") + "/tmp/test/input";
-        File file = new File(localFileLocation);
-        try {
-            file.getParentFile().mkdirs();
-            file.createNewFile();
-            FileUtils.writeStringToFile(file, "This is a test input file\n", "UTF-8");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Create an input file in tmp/test/input
+        File file = initTestFile("This is a test input file\n", "testInput");
 
         // Run script
         // Example for running a bash script correctly
-        DockerTestOutput to = stm.runSubmission("cat /input/testInput > /output/testOutput", new File[]{file});
+        DockerTestOutput to = stm.runSubmission("cat /shared/input/testInput; echo PUSH ALLOWED > /shared/output/testOutput", new File[]{file});
         assertEquals(to.logs.get(0), "This is a test input file\n");
+    }
+
+    @Test
+    void templateTest() throws InterruptedException {
+        String testOne = "@HelloWorld\n" +
+                ">Description=\"Test for hello world!\"\n" +
+                ">Required\n" +
+                "HelloWorld!";
+        String testTwo = "@HelloWorld2\n" +
+                ">Optional\n" +
+                "HelloWorld2!\n";
+        String template = testOne + "\n" + testTwo;
+
+        File[] files = new File[]{initTestFile("#!/bin/sh\necho 'HelloWorld!'", "HelloWorld.sh"),
+                initTestFile("#!/bin/sh\necho 'HelloWorld2!'", "HelloWorld2.sh")};
+
+        String script =
+                "chmod +x /shared/input/HelloWorld.sh;" +
+                "chmod +x /shared/input/HelloWorld2.sh;" +
+                "/shared/input/HelloWorld.sh > /shared/output/HelloWorld;" +
+                "/shared/input/HelloWorld2.sh > /shared/output/HelloWorld2";
+
+       // DockerSubmissionTestModel.addDocker("alpine:latest");
+        // Load docker container
+        DockerSubmissionTestModel stm = new DockerSubmissionTestModel("alpine");
+        List<DockerSubtestResult> results = stm.runSubmissionWithTemplate(script, template, files);
+
+        // Template parser testing
+        assertEquals(results.size(), 2);
+
+        assertTrue(results.get(0).isRequired());
+        assertFalse(results.get(1).isRequired());
+
+        assertEquals(results.get(0).getCorrect(), "HelloWorld!\n");
+        assertEquals(results.get(1).getCorrect(), "HelloWorld2!\n");
+
+        assertEquals(results.get(0).getTestDescription(), "Test for hello world!");
+        assertEquals(results.get(1).getTestDescription(), "");
+
+        // docker output testing
+        assertEquals(results.get(0).getOutput(), "HelloWorld!\n");
+        assertEquals(results.get(1).getOutput(), "HelloWorld2!\n");
+
     }
 
 
