cef47b916c1fbc5c6e3fd36d97080afe147bdc97
Arne Dierick <arne.dierick@ugent.be>
2024-04-17 10:14:48 +0200

    The file upload should work now on all OS's and doesn't return a 404 error now

--- frontend/src/pages/projectTest/ProjectTestPage.tsx
+++ frontend/src/pages/projectTest/ProjectTestPage.tsx
@@ -1,41 +1,48 @@
-import React, { useState } from 'react';
-import { Typography, Switch, Input, Button, Upload } from 'antd';
+import React, {useState} from 'react';
+import {Button, Input, Switch, Typography, Upload} from 'antd';
 import {InboxOutlined, LeftOutlined} from '@ant-design/icons';
 import useProject from '../../hooks/useProject';
-import { useTranslation } from 'react-i18next';
-import useApp from '../../hooks/useApp';
+import {useTranslation} from 'react-i18next';
 import {Link} from "react-router-dom";
 import useCourse from "../../hooks/useCourse";
 
-const { Title, Text } = Typography;
-const { TextArea } = Input;
+const {Title, Text} = Typography;
+const {TextArea} = Input;
+
+const programmingExtensions = [".txt", ".java", ".c", ".cpp", ".py", ".js", ".php", ".html", ".css", ".swift", ".ruby", ".r", ".go", ".kotlin", ".ts", ".scala", ".dart", ".lua", ".perl", ".vb", ".matlab", ".rust", ".shell", ".coffee", ".groovy", ".jsx", ".tsx", ".h", ".hpp", ".vb", ".pl", ".css", ".scss", ".sass", ".less", ".styl", ".ejs", ".jade", ".pug", ".handlebars", ".mustache", ".twig", ".liquid", ".slim", ".xml", ".svg", ".md", ".json", ".yaml", ".yml", ".ini", ".cfg", ".toml", ".properties", ".gradle", ".kts", ".bash", ".zsh", ".fish", ".sh", ".bat", ".cmd", ".ps1", ".awk", ".sed", ".sql", ".plsql", ".tsql", ".pgsql", ".rsql", ".sqlite", ".mysql", ".mariadb", ".postgresql", ".mongodb", ".csharp", ".cs", ".vb", ".vbnet", ".fsharp", ".fs", ".clojure", ".clj", ".emacs", ".lisp", ".elisp", ".racket", ".rkt", ".scheme", ".haskell", ".hs", ".ocaml", ".ml", ".elm", ".erlang", ".elixir", ".swift", ".actionscript", ".as", ".apex", ".crystal", ".d", ".delphi", ".objectpascal", ".forth", ".fortran", ".haxe", ".julia", ".kotlin", ".nim", ".pascal", ".purescript", ".reason", ".ruby", ".rust", ".scala", ".smalltalk", ".sqf", ".tcl", ".verilog", ".vhdl", ".v", ".wasm", ".zig"].join(",");
+
 
 // Custom component for code block
-const CodeBlock: React.FC<{ value: string; placeholder: string, onChange: (value: string) => void }> = ({ value, placeholder, onChange }) => {
-    const [fileContent, setFileContent] = useState<string>('');
+const CodeBlock: React.FC<{ value: string; placeholder: string, onChange: (value: string) => void }> = ({
+    value,
+    placeholder,
+    onChange
+                                                                                                        }) => {
 
     const handleFileUpload = (file: File) => {
         const reader = new FileReader();
         reader.onload = (e) => {
             const contents = e.target?.result as string;
-            setFileContent(contents);
             onChange(contents);
         };
         reader.readAsText(file);
+        // Prevent default upload action
+        return false;
     };
 
+
     return (
-        <div style={{ marginTop: '10px', maxHeight: '20em', overflowY: 'auto' }}>
+        <div style={{marginTop: '10px', maxHeight: '20em', overflowY: 'auto'}}>
             <TextArea
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
                 placeholder={placeholder}
-                autoSize={{ minRows: 3 }}
-                style={{ fontFamily: 'monospace', whiteSpace: 'pre', overflowX: 'auto' }}
+                autoSize={{minRows: 3}}
+                style={{fontFamily: 'monospace', whiteSpace: 'pre', overflowX: 'auto'}}
             />
-            <div style={{ marginTop: '8px', display: 'flex', justifyContent: 'flex-end' }}>
-                <Upload accept=".*" showUploadList={false} beforeUpload={handleFileUpload}>
-                    <Button icon={<InboxOutlined />}>Upload</Button>
+            <div style={{marginTop: '8px', display: 'flex', justifyContent: 'flex-end'}}>
+                <Upload accept={programmingExtensions} showUploadList={false} beforeUpload={handleFileUpload}>
+                    <Button icon={<InboxOutlined/>}>Upload</Button>
                 </Upload>
             </div>
         </div>
@@ -43,7 +50,6 @@ const CodeBlock: React.FC<{ value: string; placeholder: string, onChange: (value
 };
 
 const ProjectTestsPage: React.FC = () => {
-    const app = useApp();
     const [mode, setMode] = useState<string>('Template');
     const [enableFeatureTests, setEnableFeatureTests] = useState<boolean>(false);
     const [enableDockerTests, setEnableDockerTests] = useState<boolean>(false);
@@ -52,7 +58,7 @@ const ProjectTestsPage: React.FC = () => {
     const [dockerScriptText, setDockerScriptText] = useState<string>('');
     const [templateText, setTemplateText] = useState<string>('');
     const project = useProject();
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const course = useCourse()
 
 
@@ -70,7 +76,15 @@ const ProjectTestsPage: React.FC = () => {
 
     const handleSubmit = () => {
         // Implementeer de logica voor het submitten van de formuliergegevens
-        console.log('Formulier ingediend:', { mode, enableFeatureTests, enableDockerTests, structureTemplateText, dockerImageText, dockerScriptText, templateText });
+        console.log('Formulier ingediend:', {
+            mode,
+            enableFeatureTests,
+            enableDockerTests,
+            structureTemplateText,
+            dockerImageText,
+            dockerScriptText,
+            templateText
+        });
     };
 
     return (
