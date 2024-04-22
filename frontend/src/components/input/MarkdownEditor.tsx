fc1607a4540df68405a64a2254679c8f83aa407b
usserwoutV2 <wout.verdyck@gmail.com>
2024-04-22 18:32:02 +0200

    Woops, forgot to add this file

--- frontend/src/components/input/MarkdownEditor.tsx
+++ frontend/src/components/input/MarkdownEditor.tsx
@@ -2,19 +2,22 @@ import { Input, Tabs, TabsProps } from "antd"
 import { TextAreaProps } from "antd/lib/input"
 import { FC } from "react"
 import MarkdownTextfield from "./MarkdownTextfield"
+import { useTranslation } from "react-i18next"
 
 const MarkdownEditor: FC<{ value: string | null } & TextAreaProps> = ({ ...args }) => {
   // TODO: Implement markdown editor
+  const {t} = useTranslation()
+
 
   const items: TabsProps["items"] = [
     {
       key: "write",
-      label: "Write",
+      label: t("components.write"),
       children: <Input.TextArea  rows={4}  {...args} />,
     },
     {
       key: "preview",
-      label: "Preview",
+      label: t("components.preview"),
       children: <MarkdownTextfield content={args.value} />,
     },
   ]
