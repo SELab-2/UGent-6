14b1a01bd553cb61a36372de262e94f90c784a57
Arne Dierick <arne.dierick@ugent.be>
2024-04-24 17:16:26 +0200

    Leave Course Button isn't created for course creator

--- frontend/src/pages/course/components/LeaveCourse/LeaveCourseButton.tsx
+++ frontend/src/pages/course/components/LeaveCourse/LeaveCourseButton.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, {FC, useContext, useState} from "react";
 import { Button, Modal, Typography } from "antd";
 import { leaveCourse } from "./CourseMembershipService";
 import { useTranslation } from "react-i18next";
@@ -6,6 +6,7 @@ import {useNavigate} from "react-router-dom";
 import {AppRoutes} from "../../../../@types/routes";
 import useAppApi from "../../../../hooks/useAppApi";
 import {UsergroupDeleteOutlined} from "@ant-design/icons";
+import {CourseContext} from "../../../../router/CourseRoutes";
 
 interface LeaveCourseButtonProps {
     courseId: string;
@@ -17,6 +18,7 @@ const LeaveCourseButton: FC<LeaveCourseButtonProps> = ({ courseId }) => {
     const { t } = useTranslation();
     const [confirmLeaveVisible, setConfirmLeaveVisible] = useState<boolean>(false);
     const { message } = useAppApi()
+    const { member } = useContext(CourseContext)
 
     const handleLeaveConfirm = async () => {
         const result = await leaveCourse(courseId, t); // Pass the translation function as a parameter
@@ -37,23 +39,26 @@ const LeaveCourseButton: FC<LeaveCourseButtonProps> = ({ courseId }) => {
         setConfirmLeaveVisible(true);
     };
 
-    return (
-        <>
-            <Button type="text"
-                    onClick={showConfirmLeaveModal} icon={<UsergroupDeleteOutlined />}>
-                {t("course.leave")}
-            </Button>
-            <Modal
-                title={t("course.leave")}
-                open={confirmLeaveVisible}
-                onOk={handleLeaveConfirm}
-                onCancel={handleLeaveCancel}
-                okType="danger"
-            >
-                <Text> {t("course.leaveConfirm")} </Text>
-            </Modal>
-        </>
-    );
+    if(member.relation !== "creator") {
+        return (
+            <>
+                <Button type="text"
+                        onClick={showConfirmLeaveModal} icon={<UsergroupDeleteOutlined/>}>
+                    {t("course.leave")}
+                </Button>
+                <Modal
+                    title={t("course.leave")}
+                    open={confirmLeaveVisible}
+                    onOk={handleLeaveConfirm}
+                    onCancel={handleLeaveCancel}
+                    okType="danger"
+                >
+                    <Text> {t("course.leaveConfirm")} </Text>
+                </Modal>
+            </>
+        );
+    }
+    return null;
 };
 
 export default LeaveCourseButton;
