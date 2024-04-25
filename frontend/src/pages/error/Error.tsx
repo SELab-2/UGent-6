5e10f0e2396d35858c0401ba23a20278888d74bd
Arne Dierick <arne.dierick@ugent.be>
2024-04-25 11:06:19 +0200

    Added the possibility to give an axiosError To the ErrorPage so it can handle the rendering of the error

--- frontend/src/pages/error/Error.tsx
+++ frontend/src/pages/error/Error.tsx
@@ -1,22 +1,22 @@
-import React  from "react";
-import { AppRoutes } from "../../@types/routes"
-import {useTranslation} from "react-i18next";
-import {Button, Result, Space, Typography} from "antd";
-import { Link, useNavigate } from "react-router-dom";
-
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Button, Result, Space, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
+import { AxiosError } from "axios";
 
 interface ErrorPageProps {
     errorCode?: number;
     errorMessage?: string;
+    axiosError?: AxiosError;
 }
 
-const Error: React.FC<ErrorPageProps> = ({ errorCode, errorMessage }) => {
-    const { t } = useTranslation()
+const Error: React.FC<ErrorPageProps> = ({ errorCode, errorMessage, axiosError }) => {
+    const { t } = useTranslation();
     const { Title, Text } = Typography;
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    let title:string = "";
-    let status: "403" | "404" | "500"  = "404";
+    let title: string
+    let status: "403" | "404" | "500" = "404";
 
     // Mapping van errorCode naar status
     const statusMapping: { [key: number]: "403" | "404" | "500" } = {
@@ -25,27 +25,34 @@ const Error: React.FC<ErrorPageProps> = ({ errorCode, errorMessage }) => {
         500: "500"
     };
 
-    const textMapping: { [key: number]: string} = {
+    const textMapping: { [key: number]: string } = {
         404: t("error.404_message"),
         403: t("error.403_message"),
         500: t("error.500_message")
+    };
+
+    // If axiosError was provided, put error Info into the variables
+    if(axiosError){
+        errorCode = axiosError.response?.status || undefined;
+        if(axiosError.response !== undefined){
+            errorMessage = axiosError.response.data?.toString() || undefined
+        }
     }
 
-    // Add default message to error page when no error message was provided
-    if(errorMessage === undefined){
-        if(errorCode !== undefined){
+    // add default message to error page when no error message was provided
+    if (errorMessage === undefined) {
+        if (errorCode !== undefined) {
             errorMessage = textMapping[errorCode] || t("error.subtitle");
-        }else{
+        } else {
             errorMessage = t("error.subtitle");
         }
     }
     // Add default error title if no error code was provided, otherwise set it to error code
-    if(errorCode === undefined){
-        title = t("error.title")
-
-    }else{
-        title = errorCode.toString()
-        status = statusMapping[errorCode] || "404"
+    if (errorCode === undefined) {
+        title = t("error.title");
+    } else {
+        title = errorCode.toString();
+        status = statusMapping[errorCode] || "404";
     }
 
     return (
@@ -53,11 +60,11 @@ const Error: React.FC<ErrorPageProps> = ({ errorCode, errorMessage }) => {
             <Space direction="vertical">
                 <Title>{title}</Title>
                 <Text>{errorMessage}</Text>
-               
-                <Button onClick={()=> navigate(-1)} type="primary" >{t("error.homepage")}</Button>
+
+                <Button onClick={() => navigate(-1)} type="primary">{t("error.back")}</Button>
             </Space>
         </Result>
     );
-}
+};
 
 export default Error;
