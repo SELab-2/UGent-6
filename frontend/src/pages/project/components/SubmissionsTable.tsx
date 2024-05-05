1363344da5a27dd2983a651a230faa2ad58d1e02
Arne Dierick <arne.dierick@ugent.be>
2024-05-05 15:35:08 +0200

    Course admin can sort submissions via group id and submission time

--- frontend/src/pages/project/components/SubmissionsTable.tsx
+++ frontend/src/pages/project/components/SubmissionsTable.tsx
@@ -69,6 +69,12 @@ const SubmissionsTable: FC<{ submissions: ProjectSubmissionsType[] | null; onCha
         dataIndex: "group",
         key: "group",
         render: (g) => <Typography.Text>{g.name}</Typography.Text>,
+        sorter: (a: ProjectSubmissionsType, b: ProjectSubmissionsType) => {
+          // Implement sorting logic for group column
+          const groupA = a.group.groupId
+          const groupB = b.group.groupId
+          return groupA - groupB;
+        },
         description: "test",
       },
       {
@@ -99,6 +105,12 @@ const SubmissionsTable: FC<{ submissions: ProjectSubmissionsType[] | null; onCha
         dataIndex: "submission",
         key: "submission",
         render: (time: ProjectSubmissionsType["submission"]) => time?.submissionTime && <Typography.Text>{new Date(time.submissionTime).toLocaleString()}</Typography.Text>,
+        sorter: (a: ProjectSubmissionsType, b: ProjectSubmissionsType) => {
+          // Implement sorting logic for submissionTime column
+          const timeA: any = a.submission?.submissionTime || 0;
+          const timeB: any = b.submission?.submissionTime || 0;
+          return timeA - timeB;
+        },
       },
       {
         title: `Score (/${project?.maxScore ?? ""})`,
@@ -128,6 +140,7 @@ const SubmissionsTable: FC<{ submissions: ProjectSubmissionsType[] | null; onCha
   }, [t, project, submissions])
   return (
     <Table
+      showSorterTooltip={{mouseEnterDelay: 1}}
       loading={submissions === null}
       dataSource={submissions ?? []}
       locale={{ emptyText: submissions === null ? t("project.loadingSubmissions") : t("project.noSubmissions") }}
