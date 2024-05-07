0fd0a586e1f1cce5462f0e7971db1e03ab92bcaa
Arne Dierick <arne.dierick@ugent.be>
2024-05-07 18:13:56 +0200

    Added dropdwon to home page to only show courses from selected year

--- frontend/src/pages/index/components/HorizontalCourseScroll.tsx
+++ frontend/src/pages/index/components/HorizontalCourseScroll.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Space, Typography } from "antd"
+import { Button, Select, Space, Typography } from "antd"
 import useUser from "../../../hooks/useUser"
 import CourseCard from "./CourseCard"
 import { FC, useEffect, useState } from "react"
@@ -8,8 +8,9 @@ import { PlusOutlined, RightOutlined } from "@ant-design/icons"
 import { ProjectsType } from "../Home"
 import TeacherView from "../../../hooks/TeacherView"
 import { useNavigate } from "react-router-dom"
-import AppRouter from "../../../router/AppRouter"
 import { AppRoutes } from "../../../@types/routes"
+
+const { Option } = Select;
 
 export type CourseProjectsType = {
   [courseId: string]: {
@@ -23,6 +24,8 @@ const HorizontalCourseScroll: FC<{ projects: ProjectsType | null; onOpenNew: () 
   const [courseProjects, setCourseProjects] = useState<CourseProjectsType | null>(null)
   const [adminCourseProjects, setAdminCourseProjects] = useState<CourseProjectsType | null>(null)
   const [archivedCourses, setArchivedCourses] = useState<boolean>(false)
+  const [selectedYear, setSelectedYear] = useState<number>(null)
+
   const { t } = useTranslation()
   const navigate = useNavigate()
 
@@ -55,6 +58,11 @@ const HorizontalCourseScroll: FC<{ projects: ProjectsType | null; onOpenNew: () 
       }
     })
 
+    if (courses && courses.length > 0) {
+      const allYears = courses.map(course => course.year);
+      setSelectedYear(Math.max(...allYears))
+    }
+
     setCourseProjects(courseProjects)
     setAdminCourseProjects(adminCourseProjects)
     setArchivedCourses(hasArchivedCourses)
@@ -62,113 +70,119 @@ const HorizontalCourseScroll: FC<{ projects: ProjectsType | null; onOpenNew: () 
     return () => (ignore = true)
   }, [courses, projects])
 
-  const courseProjectsArray = courseProjects ? Object.values(courseProjects) : []
-  const adminCourseProjectsArray = adminCourseProjects ? Object.values(adminCourseProjects) : []
+  // Filter courses based on selected year
+  const filteredCourseProjects = courseProjects ? Object.values(courseProjects).filter(course => course.course.year === selectedYear) : [];
+  const filteredAdminCourseProjects = adminCourseProjects ? Object.values(adminCourseProjects).filter(course => course.course.year === selectedYear) : [];
+
+  // Generate options for the year dropdown
+  const yearOptions = Array.from(new Set([...(courses || []).map(course => course.year)]));
 
   return (
-    <>
-      {courseProjects && adminCourseProjectsArray.length && !courseProjectsArray.length ? null : (
-        <Typography.Title
-          level={3}
-          style={{
-            paddingLeft: "2rem",
-          }}
-        >
-          {t("home.yourCourses")}
-         {adminCourseProjectsArray.length === 0 && <TeacherView>
-            <Button
-              onClick={onOpenNew}
-              type="text"
-              style={{ marginLeft: "1rem" }}
-              icon={<PlusOutlined />}
-            />
-          </TeacherView>}
-
-          {(archivedCourses || courseProjectsArray.length > 2) && <Button
-              type="link"
-              style={{ float: "right" }}
-              onClick={() => navigate(+AppRoutes.COURSES+"?role=enrolled")}
-            >
-              {t("home.moreCourses")} <RightOutlined />
-            </Button>}
-        </Typography.Title>
-      )}
-
-      {courseProjects && !adminCourseProjectsArray.length && !courseProjectsArray.length && (
-        <Typography.Text
-          style={{
-            paddingLeft: "2rem",
-          }}
-        >
-          {t("home.noCourses")}
-        </Typography.Text>
-      )}
-      <Space
-        className="small-scroll-bar"
-        style={{ maxWidth: "100%", overflowX: "auto", whiteSpace: "nowrap", padding: "10px 2rem" }}
-      >
-        {courseProjects !== null
-          ? courseProjectsArray.map((c) => (
-              <CourseCard
-                key={c.course.courseId}
-                courseProjects={c}
-              />
-            ))
-          : Array(3)
-              .fill(0)
-              .map((_, i) => (
-                <Card
-                  key={i}
-                  loading
-                  style={{ width: 300, height: 235 }}
-                />
-              ))}
-      </Space>
-
-      {adminCourseProjects && !!adminCourseProjectsArray.length && (
-        <>
-          <Typography.Title
-            level={3}
-            style={{
-              paddingLeft: "2rem",
-            }}
+      <>
+        {/* Dropdown for selecting year */}
+        {yearOptions.length > 1 && (
+        <div style={{paddingLeft: '1rem', marginBottom: 0, paddingBottom:0}}>
+          <Select
+              variant='borderless'
+              value={selectedYear}
+              onChange={(value: number) => setSelectedYear(value)}
+              style={{width: 120}}
           >
-            {t("home.myCourses")}
-
-            {courseProjects && (
-              <TeacherView>
-                <Button
-                  onClick={onOpenNew}
-                  type="text"
-                  style={{ marginLeft: "1rem" }}
-                  icon={<PlusOutlined />}
-                />
-              </TeacherView>
-            )}
-
-            {(archivedCourses || adminCourseProjectsArray.length > 2 )&& <Button
-              type="link"
-              style={{ float: "right" }}
-              onClick={() => navigate(AppRoutes.COURSES+"?role=admin")}
-            >
-              {t("home.moreCourses")} <RightOutlined />
-            </Button>}
-          </Typography.Title>
-          <Space
-            className="small-scroll-bar"
-            style={{ maxWidth: "100%", overflowX: "auto", whiteSpace: "nowrap", padding: "10px 2rem", alignItems: "stretch" }}
-          >
-            {adminCourseProjectsArray.map((c) => (
-              <CourseCard
-                key={c.course.courseId}
-                courseProjects={c}
-                adminView
-              />
+            {yearOptions.map(year => (
+                <Option key={year} value={year}>{`${year} - ${year + 1}`}</Option>
             ))}
-          </Space>
-        </>
-      )}
-    </>
+          </Select>
+        </div>
+        )}
+
+        {filteredCourseProjects.length > 0 && (
+            <>
+              <Typography.Title
+                  level={3}
+                  style={{
+                    paddingLeft: "2rem",
+                    marginTop: 0,
+                    paddingTop: 0
+                  }}
+              >
+                {t("home.yourCourses")}
+              </Typography.Title>
+              <Space
+                  className="small-scroll-bar"
+                  style={{
+                    display: 'flex',
+                    maxWidth: "100%",
+                    overflowX: "auto",
+                    whiteSpace: "nowrap",
+                    padding: "10px 2rem"
+                  }}
+              >
+                {filteredCourseProjects.map((c) => (
+                    <div key={c.course.courseId} style={{height: '100%'}}>
+                      <CourseCard courseProjects={c}/>
+                    </div>
+                ))}
+              </Space>
+            </>
+        )}
+
+        {filteredAdminCourseProjects && filteredAdminCourseProjects.length > 0 && (
+            <>
+              <Typography.Title
+                  level={3}
+                  style={{
+                    paddingLeft: "2rem",
+                  }}
+              >
+                {t("home.myCourses")}
+
+                {courseProjects && (
+                    <TeacherView>
+                      <Button
+                          onClick={onOpenNew}
+                          type="text"
+                          style={{ marginLeft: "1rem",
+                                   marginTop: 0,
+                                   paddingTop: 0
+                          }}
+                          icon={<PlusOutlined />}
+                      />
+                    </TeacherView>
+                )}
+
+                {(archivedCourses || filteredAdminCourseProjects.length > 2 )&& <Button
+                    type="link"
+                    style={{ float: "right" }}
+                    onClick={() => navigate(AppRoutes.COURSES+"?role=admin")}
+                >
+                  {t("home.moreCourses")} <RightOutlined />
+                </Button>}
+              </Typography.Title>
+              <Space
+                  className="small-scroll-bar"
+                  style={{ display: 'flex', maxWidth: "100%", overflowX: "auto", whiteSpace: "nowrap", padding: "10px 2rem", alignItems: "stretch" }}
+              >
+                {filteredAdminCourseProjects.map((c) => (
+                    <CourseCard
+                        key={c.course.courseId}
+                        courseProjects={c}
+                        adminView
+                    />
+                ))}
+              </Space>
+            </>
+        )}
+
+        {filteredCourseProjects.length === 0 && filteredAdminCourseProjects.length === 0 && (
+            <Typography.Text
+                style={{
+                  paddingLeft: "2rem",
+                }}
+            >
+              {t("home.noCourses")}
+            </Typography.Text>
+        )}
+      </>
   )
 }
 
