1630a1581218c951b72a3999393fe98e3b790e69
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2024-05-10 11:10:12 +0200

    hopefully files are excluded properly right now

--- backend/app/src/test/java/com/ugent/pidgeon/docker/DockerSubmissionTestTest.java
+++ backend/app/src/test/java/com/ugent/pidgeon/docker/DockerSubmissionTestTest.java
@@ -1,4 +1,4 @@
-package com.ugent.pidgeon.model;
+package com.ugent.pidgeon.docker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
