355ff0b3c947b9f4a13aad30b12578cf7fc44612
Arne Dierick <arne.dierick@ugent.be>
2024-05-18 16:52:47 +0200

    Added UI in the projectTable (where useIsCourseAdmin is defined) so a course admin can easily check the visibility of projects

--- frontend/src/pages/index/components/ProjectCard.tsx
+++ frontend/src/pages/index/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect, useState } from "react"
-import ProjectTable, { ProjectType } from "./ProjectTable"
+import ProjectTableCourse, { ProjectType } from "./ProjectTableCourse"
+import ProjectTable, { ProjectType as NormalProjectType } from "./ProjectTable"
 import { Button, Card } from "antd"
 import { ApiRoutes } from "../../../@types/requests.d"
 import { useTranslation } from "react-i18next"
@@ -8,53 +9,64 @@ import { useNavigate } from "react-router-dom"
 import CourseAdminView from "../../../hooks/CourseAdminView"
 import { PlusOutlined } from "@ant-design/icons"
 import useApi from "../../../hooks/useApi"
+import useIsCourseAdmin from "../../../hooks/useIsCourseAdmin";
 
 const ProjectCard: FC<{ courseId?: number }> = ({ courseId }) => {
-  const [projects, setProjects] = useState<ProjectType[] | null>(null)
-  const { t } = useTranslation()
-  const navigate = useNavigate()
-  const API = useApi()
+    const [projects, setProjects] = useState<ProjectType[] | null>(null)
+    const { t } = useTranslation()
+    const navigate = useNavigate()
+    const API = useApi()
+    const isCourseAdmin = useIsCourseAdmin()
 
-  useEffect(() => {
-    if (courseId) {
-      API.GET(ApiRoutes.COURSE_PROJECTS, { pathValues: { id: courseId } }).then((res) => {
-        if (!res.success) return
-        setProjects(res.response.data)
-      })
-    }
-  }, [courseId])
+    useEffect(() => {
+        if (courseId) {
+            API.GET(ApiRoutes.COURSE_PROJECTS, { pathValues: { id: courseId } }).then((res) => {
+                if (!res.success) return
+                setProjects(res.response.data)
+            })
+        }
+    }, [courseId])
 
-  return (
-    <>
-      <CourseAdminView>
-        <div style={{ textAlign: "right", paddingBottom: "10px" }}>
-          <Button
-            onClick={() => navigate(AppRoutes.PROJECT_CREATE.replace(":courseId", String(courseId)))}
-            icon={<PlusOutlined />}
-            type="primary"
-          >
-            {t("project.newProject")}
-          </Button>
-        </div>
-      </CourseAdminView>
-      <Card
-        style={{
-          width: "100%",
-          overflow: "auto",
-        }}
-        styles={{
-          body: {
-            padding: "0",
-          },
-        }}
-      >
-        <ProjectTable
-          ignoreColumns={["course"] }
-          projects={projects}
-        />
-      </Card>
-    </>
-  )
+    return (
+        <>
+            {isCourseAdmin && (
+                <CourseAdminView>
+                    <div style={{ textAlign: "right", paddingBottom: "10px" }}>
+                        <Button
+                            onClick={() => navigate(AppRoutes.PROJECT_CREATE.replace(":courseId", String(courseId)))}
+                            icon={<PlusOutlined />}
+                            type="primary"
+                        >
+                            {t("project.newProject")}
+                        </Button>
+                    </div>
+                </CourseAdminView>
+            )}
+            <Card
+                style={{
+                    width: "100%",
+                    overflow: "auto",
+                }}
+                styles={{
+                    body: {
+                        padding: "0",
+                    },
+                }}
+            >
+                {isCourseAdmin ? (
+                    <ProjectTableCourse
+                        ignoreColumns={["course"] }
+                        projects={projects}
+                    />
+                ) : (
+                    <ProjectTable
+                        ignoreColumns={["course"] }
+                        projects={projects as NormalProjectType[]}
+                    />
+                )}
+            </Card>
+        </>
+    )
 }
 
 export default ProjectCard
