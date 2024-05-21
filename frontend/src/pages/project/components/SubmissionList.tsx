bf2be713731a485c22775f7fd017ba9b88488b6f
Floris Kornelis van Dijken <badduckssb@gmail.com>
2024-05-21 23:24:49 +0200

    done

--- frontend/src/pages/project/components/SubmissionList.tsx
+++ frontend/src/pages/project/components/SubmissionList.tsx
@@ -1,5 +1,5 @@
 import { Button, List, Table, TableProps, Typography } from "antd"
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { Link, useParams } from "react-router-dom"
 import SubmissionStatusTag, { createStatusBitVector } from "./SubmissionStatusTag"
 import { GroupSubmissionType } from "./SubmissionTab"
@@ -9,21 +9,20 @@ import { AppRoutes } from "../../../@types/routes"
 
 
 
-const SubmissionList: FC<{ submissions: GroupSubmissionType[] | null }> = ({ submissions }) => {
+const SubmissionList: FC<{ submissions: GroupSubmissionType[] | null, indices: Map<Number, Number> }> = ({ submissions, indices }) => {
   const {t} = useTranslation()
   const {courseId} = useParams()
   
-
   const columns: TableProps['columns'] = [
     {
       title: t("project.submission"),
       key: "submissionId",
-      sorter: (a: GroupSubmissionType, b: GroupSubmissionType) => {
-        return a.submissionId - b.submissionId
-      },
+      //sorter: (a: GroupSubmissionType, b: GroupSubmissionType) => {
+      //  return a.submissionId - b.submissionId
+      //},
       render: (submission: GroupSubmissionType, _, index:number) => (
         <Link to={AppRoutes.SUBMISSION.replace(":courseId",courseId!).replace(":projectId",submission.projectId+"").replace(":submissionId",submission.submissionId+"")}>
-          <Button type="link">#{index+1}</Button>
+          <Button type="link">#{indices ? indices.get(submission.submissionId)?.toString() : ""}</Button>
         </Link>
       ),
     },
@@ -31,7 +30,7 @@ const SubmissionList: FC<{ submissions: GroupSubmissionType[] | null }> = ({ sub
       title: t("project.submissionTime"),
       dataIndex: "submissionTime",
       key: "submissionTime",
-      
+      sorter: (a: GroupSubmissionType, b: GroupSubmissionType) => new Date(a.submissionTime).getTime() - new Date(b.submissionTime).getTime(),
       render: (submission: GroupSubmissionType["submissionTime"]) => (
         <Typography.Text>{new Date(submission).toLocaleString()}</Typography.Text>
       ),
--- frontend/src/pages/project/components/SubmissionTab.tsx
+++ frontend/src/pages/project/components/SubmissionTab.tsx
@@ -8,6 +8,7 @@ export type GroupSubmissionType = GET_Responses[ApiRoutes.PROJECT_GROUP_SUBMISSI
 
 const SubmissionTab: FC<{ projectId: number; courseId: number; testSubmissions?: boolean }> = ({ projectId, courseId, testSubmissions }) => {
   const [submissions, setSubmissions] = useState<GroupSubmissionType[] | null>(null)
+  const [indices, setIndices] = useState<Map<number, number>>(new Map())
   const project = useProject()
   const API = useApi()
 
@@ -18,7 +19,16 @@ const SubmissionTab: FC<{ projectId: number; courseId: number; testSubmissions?:
     let ignore = false
     API.GET(testSubmissions ? ApiRoutes.PROJECT_TEST_SUBMISSIONS : ApiRoutes.PROJECT_GROUP_SUBMISSIONS, { pathValues: { projectId: project.projectId, groupId: project.groupId ?? "" } }).then((res) => {
       if (!res.success || ignore) return
-      setSubmissions(res.response.data.sort((a, b) => b.submissionId - a.submissionId))
+      console.log(res.response.data)
+      //this is sorts the submissions by submission time, with the oldest submission first
+      const ascending = res.response.data.sort((a, b) => new Date(a.submissionTime).getTime() - new Date(b.submissionTime).getTime())
+      const tmp = new Map()
+      ascending.forEach((submission, index) => {
+        tmp.set(submission.submissionId, index+1)
+      })
+      setIndices(tmp)
+      //we need descending order, so we reverse the array
+      setSubmissions(ascending.reverse())
     })
 
     return () => {
@@ -30,7 +40,7 @@ const SubmissionTab: FC<{ projectId: number; courseId: number; testSubmissions?:
     <>
  
 
-      <SubmissionList submissions={submissions} />
+      <SubmissionList submissions={submissions} indices={indices} />
     </>
   )
 }
