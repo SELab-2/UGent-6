ab468a299ad6fa2a9c826cb3b454d5019a1c225a
Arthur Werbrouck <Arthur.jasaw@gmail.com>
2024-05-22 10:54:49 +0200

    removed broken tests

--- frontend/cypress/components/GroupTab.cy.tsx
+++
@@ -1,8 +0,0 @@
-import GroupTab from '../../src/pages/project/components/GroupTab'
-
-describe('GroupTab', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<GroupTab />).should("exist")
-  })
-})
